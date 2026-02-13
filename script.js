8a4a448593e5d9bb50fb4b478dbe197b7cafe0fc
prasanth844 <135524731+prasanth844@users.noreply.github.com>
2026-02-13 14:01:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
-// complete the given function
-
-function palindrome(str){
-
+function palindrome(str) {
+    // Remove non-alphanumeric characters and convert to lowercase
+    const cleaned = str.replace(/[^a-z0-9]/gi, "").toLowerCase();
+    
+    // Reverse the cleaned string
+    const reversed = cleaned.split("").reverse().join("");
+    
+    // Compare
+    return cleaned === reversed;
 }
-module.exports = palindrome
+
+module.exports = palindrome;
